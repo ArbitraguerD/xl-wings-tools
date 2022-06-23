1f97abde82aa0f36f4d82b5bd00ccdf96339ef64
ArbitraguerD <49497288+ArbitraguerD@users.noreply.github.com>
2022-06-22 20:21:43 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xl-wings-tools',
+    version='',
+    packages=['xl-wings-stuff'],
+    url='',
+    license='',
+    author='dpara',
+    author_email='',
+    description=''
+)
---
+++ xl-wings-stuff/test-1.py
@@ -0,0 +1,2 @@
+def testDp():
+    return 'abcdef'
